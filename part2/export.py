68938cca74964204697dcf9ad668e9a926a7cdf7
Molnar Arpad <arpad.molnarx@gmail.com>
2017-06-27 13:01:20 +0200

    finished all tasks

--- part2/export.py
+++ part2/export.py
@@ -1,2 +1,26 @@
 
 # Export functions
+
+from reports import *
+
+
+def export_all():
+
+    exp_file = open("expfile.txt", "w")
+    exp_file.writelines(str(get_most_played(None)) + "\n")
+    exp_file.writelines(str(sum_sold(None)) + "\n")
+    exp_file.writelines(str(get_selling_avg(None)) + "\n")
+    exp_file.writelines(str(count_longest_title(None)) + "\n")
+    exp_file.writelines(str(get_date_avg(None)) + "\n")
+    exp_file.writelines(str(get_game(None, "Counter-Strike")) + "\n")
+    exp_file.writelines(str(count_grouped_by_genre(None)) + "\n")
+    exp_file.writelines(str(get_date_ordered(None)) + "\n")
+    exp_file.close()
+
+
+def main():
+    export_all()
+
+
+if __name__ == "__main__":
+    main()
--- part2/reports.py
+++ part2/reports.py
@@ -1,2 +1,83 @@
 
 # Report functions
+
+import csv
+import operator
+
+
+def import_data(file_name=None):
+    if file_name is None:
+        file_name = "game_stat.txt"
+    with open(file_name, "r") as data:
+        reader = csv.reader(data, delimiter="\t")
+        game_data = []
+        for row in reader:
+            game_data.append(row)
+        return game_data
+
+
+def get_most_played(file_name):
+    most_played, played = None, 0
+    for game in import_data(file_name):
+        if float(game[1]) > played:
+            most_played, played = game[0], float(game[1])
+    return most_played
+
+
+def sum_sold(file_name):
+    total = 0
+    for game in import_data(file_name):
+        total += float(game[1])
+    return total
+
+
+def get_selling_avg(file_name):
+    return sum_sold(file_name) / len(import_data(file_name))
+
+
+def count_longest_title(file_name):
+    char, name = 0, None
+    for game in import_data(file_name):
+        if len(game[0]) > char:
+            char, name = len(game[0]), game[0]
+
+    return len(name)
+
+
+def get_date_avg(file_name):
+    rel_date = 0
+    for game in import_data(file_name):
+        rel_date += int(game[2])
+    return round(rel_date / len(import_data(file_name)), 0)
+
+
+def get_game(file_name, title):
+    for game in import_data(file_name):
+        if game[0] == title:
+            return [str(game[0]), float(game[1]), int(game[2]), str(game[3]), str(game[4])]
+
+
+def count_grouped_by_genre(file_name):
+    answer = {}
+    for game in import_data(file_name):
+        if game[3] in answer:
+            answer[game[3]] += 1
+        else:
+            answer[game[3]] = 1
+    return answer
+
+
+def get_date_ordered(file_name):
+    answer = []
+    for game in sorted(import_data(file_name), key=lambda x: (-int(x[2]), x[0])):
+        answer.append(game[0])
+    return answer
+
+
+def main():
+    print(get_date_ordered(None))
+    pass
+
+
+if __name__ == "__main__":
+    main()
